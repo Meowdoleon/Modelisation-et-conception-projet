b3b2a5d13160f01de913b14ecf372fa3c5d80961
Ensiferus <berj92@uqo.ca>
2018-11-20 22:13:24 -0500

    Rajout nom label et certain get-set
    Rajouter tous les noms des labels et des textBox qu'on a besoin et certaines méthodes get et set dans LocationPanel

---
+++ LocationDeVoiture/LocationDeVoiture/src/locationdevoiture/Controller.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package locationdevoiture;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Julien
+ */
+public class Controller {
+    
+    public static void connection(JPanel present, JPanel next)
+    {
+        present.setVisible(false);
+        next.setVisible(true);
+    }
+    
+    public static void location(JPanel present, JPanel next)
+    {
+        present.setVisible(false);
+        next.setVisible(true);
+    }
+    
+    public static void retourLocation(JPanel present, JPanel next)
+    {
+        present.setVisible(false);
+        next.setVisible(true);
+    }
+    
+    public static void deconnection(JPanel present, JPanel next)
+    {
+        present.setVisible(false);
+        next.setVisible(true);
+    }
+    
+    public static void retourAcceuil(JPanel present, JPanel next)
+    {
+        present.setVisible(false);
+        next.setVisible(true);
+    }
+    
+    public static void confirmationLocation(JPanel present, JPanel next)
+    {
+        present.setVisible(false);
+        next.setVisible(true);
+    }
+    
+    public static void confirmationRetour(JPanel present, JPanel next)
+    {
+        present.setVisible(false);
+        next.setVisible(true);
+    }
+    
+    public static void calculLocation(JPanel calcul)
+    {
+        calcul.setVisible(true);
+    }
+    
+    public static void choixVehicule(JPanel vehicule)
+    {
+        vehicule.setVisible(true);
+    }
+}
---
+++ LocationDeVoiture/LocationDeVoiture/src/locationdevoiture/LocationDeVoiture.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package locationdevoiture;
+
+/**
+ *
+ * @author Julien
+ */
+public class LocationDeVoiture {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LocationDeVoitureFrame ldv = new LocationDeVoitureFrame();
+        ldv.setVisible(true);
+    }
+    
+}
